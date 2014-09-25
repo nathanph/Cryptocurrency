f641efb3841f4e76fae10029ac95cc1ee93a79f9
Nathan Hernandez <email@nathanph.com>
2014-09-25 16:31:57 -0400

    Added Transaction signature

--- Transaction.py
+++ Transaction.py
@@ -1,1 +1,14 @@
 __author__ = 'Nathan Hernandez'
+
+class Transaction:
+    def __init__(self,
+                 version,
+                 tx_in_count, tx_in_array,
+                 tx_out_count, tx_out_array,
+                 lock_time):
+        self.version = version
+        self.tx_in_count = tx_in_count
+        self.tx_out_count = tx_out_count
+        self.tx_out_array = tx_out_array
+        self.lock_time = lock_time
+
